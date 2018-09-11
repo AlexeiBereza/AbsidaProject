9ee0b89841686bfd05779eb3e53b6b42ba7679c3
Alexei <bereza.pro@gmail.com>
2018-09-11 23:01:45 +0300

    Starting point in the project

---
+++ app/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <title>Официальный сайт компании Абсида</title>
+</head>
+<body>
+    <header class="site-header wrapper">
+        <div class="site-logo">
+            <p class="site-logo__img">Site logo</p>
+        </div>
+        <nav class="primary-nav">
+                
+            <ul class="primary-nav__list">
+                <li><a href="#"><i class="fas fa-home primary-nav__home"></i></a></li>
+                <li class="primary-nav__item"><a href="#">О нас</a></li>
+                <li class="primary-nav__item"><a href="#">Новости</a></li>
+                <li class="primary-nav__item"><a href="#">Проекты</a></li>
+                <li class="primary-nav__item"><a href="#">Услуги</a></li>
+                <li class="primary-nav__item"><a href="#">Контакты</a></li>
+                <li class="primary-nav__item"><a href="#">Контакты</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="large-hero">
+        <div class="large-hero__motto">
+            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quas repudiandae corrupti omnis, animi optio!</p>
+        </div>
+        <div class="large-hero__info">
+            <div class="large-hero__info-container">
+                <div class="large-hero__box">
+                    <i class="fas fa-phone"></i>
+                    <div class="large-hero__box-text">
+                        <h4>Номер телефона:</h4>
+                        <span>8-800-495-333-44-55</span>
+                    </div>
+                </div>
+                <div class="large-hero__box">
+                    <i class="far fa-clock"></i>
+                    <div class="large-hero__box-text">
+                        <h4>Время работы:</h4>
+                        <span>Пн - Пт: 9:00 - 18:00</span>
+                    </div>
+                </div>
+                <div class="large-hero__box">
+                    <i class="fas fa-envelope"></i>
+                    <div class="large-hero__box-text">
+                        <h4>E-mail адрес:</h4>
+                        <span>absida@company.com</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
