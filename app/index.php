421ac0461b22e90cdb36a6cc5ee6fe929eb3a563
Alexei <bereza.pro@gmail.com>
2018-09-12 16:17:50 +0300

    About us and Our partners added

--- app/index.php
+++ app/index.php
@@ -38,7 +38,7 @@
                     <i class="fas fa-phone"></i>
                     <div class="large-hero__box-text">
                         <h4>Номер телефона:</h4>
-                        <span>8-800-495-333-44-55</span>
+                        <span>8-800-333-44-55</span>
                     </div>
                 </div>
                 <div class="large-hero__box">
@@ -58,5 +58,143 @@
             </div>
         </div>
     </div>
+
+    <div class="page-section">
+        <div class="wrapper page-section__flex">
+            <div class="twothirds-container">
+                <h2 class="twothirds-container__title section-title">Последние проекты</h2>
+                <div class="news-box">
+                    <div class="news-box__img">
+                        <img src="../public/assets/images/bridge-small1.jpeg" alt="">
+                        <span class="news-box__date">23 июня 2018</span>
+                    </div>
+                    <div class="news-box__content">
+                        <h2><a href="#">Lorem ipsum dolor sit amet consectetur.<a></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fuga quibusdam ullam, voluptate quaerat amet debitis voluptas excepturi asperiores distinctio libero dolores nemo sint cum reiciendis quidem nesciunt officia fugiat.</p>
+                        <a href="#" class=" btn btn__more">Подробнее ></a>
+                    </div>
+                </div>
+                <div class="news-box">
+                    <div class="news-box__img">
+                        <img src="../public/assets/images/bridge-small2.jpeg" alt="">
+                        <span class="news-box__date">5 августа 2018</span>
+                    </div>
+                    <div class="news-box__content">
+                        <h2><a href="#">Lorem ipsum dolor sit amet consectetur.<a></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fuga quibusdam ullam, voluptate quaerat amet debitis voluptas excepturi asperiores distinctio libero dolores nemo sint cum reiciendis quidem nesciunt officia fugiat.</p>
+                        <a href="#" class=" btn btn__more">Подробнее ></a>
+                    </div>
+                </div>
+            </div>
+            <div class="onethird-container">
+                <div class="news-feed">
+                    <a href="#">
+                        <div class="news-feed__box">
+                            <h3 class="news-feed__title">Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+                            <span class="news-feed__date">13.04.2018</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="news-feed__box">
+                            <h3 class="news-feed__title">Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+                            <span class="news-feed__date">25.08.2016</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="news-feed__box">
+                            <h3 class="news-feed__title">Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+                            <span class="news-feed__date">02.01.2016</span>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="news-feed__box">
+                            <h3 class="news-feed__title">Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+                            <span class="news-feed__date">30.09.2011</span>
+                        </div>
+                    </a>
+                    <a class="btn btn__more" href="#">
+                        <div class="news-feed__box">
+                            Все новости >
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-section page-section__dark about-us ">
+        <div class="wrapper page-section__flex">
+            <div class="about-us__box">
+                <h3>О нашей компании</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur beatae possimus totam numquam a praesentium iure quaerat quos, nulla officia veniam delectus, quo, voluptatibus molestiae!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, fugiat doloribus minus tempore pariatur enim tenetur eveniet eum repellendus, perferendis delectus dolor eos minima. Deleniti saepe voluptatum architecto, voluptas alias odit, consectetur sit nobis corporis quas pariatur libero, quasi nostrum.</p>
+            </div>
+            <div class="about-us__box">
+                <h3>Наши преимущества</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur beatae possimus totam numquam a praesentium iure quaerat quos, nulla officia veniam delectus, quo, voluptatibus molestiae!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, fugiat doloribus minus tempore pariatur enim tenetur eveniet eum repellendus, perferendis delectus dolor eos minima. Deleniti saepe voluptatum architecto, voluptas alias odit, consectetur sit nobis corporis quas pariatur libero, quasi nostrum.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-section">
+        <div class="wrapper">
+            <h2 class="section-title">Наши клиенты</h2>
+            <div class="partner-images page-section__flex">
+                <div class="partner-images__box">
+                    <img src="../public/assets/images/logos/logo1.jpg" alt="">
+                </div>
+                <div class="partner-images__box">
+                <img src="../public/assets/images/logos/logo2.jpg" alt="">
+                </div>
+                <div class="partner-images__box">
+                <img src="../public/assets/images/logos/logo3.jpg" alt="">
+                </div>
+                <div class="partner-images__box">
+                <img src="../public/assets/images/logos/logo4.jpg" alt="">
+                </div>
+                <div class="partner-images__box">
+                <img src="../public/assets/images/logos/logo5.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer>
+        <div class="main-footer">
+            <div class="main-footer__logo">
+                <p>Site Logo</p>
+            </div>
+            <div class="main-footer__links">
+                    <h3>Ссылки</h3>
+                <ul>
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">Проекты</a></li>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">Отзывы</a></li>
+                </ul>
+            </div>
+            <div class="main-footer__links">
+                    <h3>Наши Услуги</h3>
+                <ul>
+                    <li><a href="#">Проектировка</a></li>
+                    <li><a href="#">Производство</a></li>
+                    <li><a href="#">Lorem, ipsum.</a></li>
+                    <li><a href="#">Lorem, ipsum.</a></li>
+                </ul>
+            </div>
+            <div class="main-footer__links">
+                    <h3>Наши Контакты</h3>
+                <ul>
+                    <li><i class="fas fa-map-marker-alt"></i>Локация: г.Москва...</li>
+                    <li><i class="fas fa-phone"></i>Телефон: 8-800-333-44-55</li>
+                    <li><i class="fas fa-envelope"></i>E-mail: absida@company.com</li>
+                    <li><i class="far fa-clock"></i>Время работы: Пн - Пт: 9:00 - 18:00</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
